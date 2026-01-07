e5dbb11154b76bb91baed14615f0906aa856c0be
bob frankel <bob@biosbob.biz>
2026-01-06 19:29:02 -0800

    force readonly editors

--- src/extension.ts
+++ src/extension.ts
@@ -26,10 +26,30 @@ export async function activate(ctx: Vsc.ExtensionContext) {
             SemTok.legend()
         )
     )
+
+    const onEditor = async (ted?: Vsc.TextEditor) => {
+        if (!ted) return
+        if (ted.document.uri.scheme !== 'file' && ted.document.uri.scheme !== 'vscode-vfs') return
+        await setReadonlyIfPossible()
+    }
+
+    ctx.subscriptions.push(Vsc.window.onDidChangeActiveTextEditor(onEditor))
+    ctx.subscriptions.push(Vsc.workspace.onDidOpenTextDocument(async () => setReadonlyIfPossible()))
+
+    void onEditor(Vsc.window.activeTextEditor)
+
     await Vsc.workspace.getConfiguration('files').update('exclude', EXCLUDES, Vsc.ConfigurationTarget.Workspace)
     await Vsc.workspace.getConfiguration().update('workbench.colorTheme', 'EM•Script Dark', Vsc.ConfigurationTarget.Workspace)
     await Vsc.workspace.getConfiguration().update('git.enabled', false, Vsc.ConfigurationTarget.Workspace)
 }
 
 export function deactivate() {
+}
+
+async function setReadonlyIfPossible(): Promise<void> {
+    try {
+        await Vsc.commands.executeCommand('workbench.action.files.setActiveEditorReadonlyInSession')
+    } catch {
+        // ignore
+    }
 }
